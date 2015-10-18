7b2c925239d96b6d7b7c0c95200dbceb30ebc920
Witold Markowski <witold.a.markowski@gmail.com>
2015-10-18 19:43:49 +0200

    Introduce abstract grap window example.

---
+++ src/main/java/vtech/iot/mesh/sim/examples/AbstractGraphExample.java
@@ -0,0 +1,94 @@
+package vtech.iot.mesh.sim.examples;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.DynamicTimeSeriesCollection;
+import org.jfree.data.time.Second;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+import vtech.iot.mesh.sim.MeshSim;
+import vtech.iot.mesh.sim.domain.devices.AlohaDevice;
+
+public abstract class AbstractGraphExample extends ApplicationFrame {
+
+  private static final float Y_MAX = 100;
+  private static final float Y_MIN = 0;
+  private static final int COUNT = 2 * 60;
+  private static final int FAST = 1000;
+  private Timer timer;
+
+  public AbstractGraphExample(String windowTitle) {
+    super(windowTitle);
+
+    init();
+  }
+
+  protected abstract String getGraphTitle();
+
+  protected abstract int getSeriesCount();
+
+  protected abstract String getSeriesName(int seriesIndex);
+
+  protected abstract float getSeriesData(int seriesIndex);
+
+  private void init() {
+    this.setTitle(getGraphTitle());
+
+    final DynamicTimeSeriesCollection dataset = new DynamicTimeSeriesCollection(getSeriesCount(), COUNT, new Second());
+    dataset.setTimeBase(new Second(0, 0, 0, 1, 1, 2011));
+
+    for (int q = 0; q < getSeriesCount(); q++) {
+      dataset.addSeries(new float[] {}, q, getSeriesName(q));
+    }
+
+    JFreeChart chart = createChart(dataset);
+
+    this.add(new ChartPanel(chart), BorderLayout.CENTER);
+    JPanel btnPanel = new JPanel(new FlowLayout());
+
+    this.add(btnPanel, BorderLayout.SOUTH);
+
+    timer = new Timer(FAST, new ActionListener() {
+
+      float[] newData = new float[getSeriesCount()];
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+
+        for (int q = 0; q < getSeriesCount(); q++) {
+          newData[q] = getSeriesData(q);
+        }
+        
+        dataset.advanceTime();
+        dataset.appendData(newData);
+      }
+    });
+    
+    timer.start();
+  }
+
+  private JFreeChart createChart(final XYDataset dataset) {
+    final JFreeChart result = ChartFactory.createTimeSeriesChart(getGraphTitle(), "hh:mm:ss", "[%]", dataset, true, true, false);
+    final XYPlot plot = result.getXYPlot();
+    ValueAxis domain = plot.getDomainAxis();
+    domain.setAutoRange(true);
+    ValueAxis range = plot.getRangeAxis();
+    range.setRange(Y_MIN, Y_MAX);
+    range.setAutoRange(true);
+    return result;
+  }
+}
--- src/main/java/vtech/iot/mesh/sim/examples/AlohaTransmitters.java
+++ src/main/java/vtech/iot/mesh/sim/examples/AlohaTransmitters.java
@@ -1,81 +1,50 @@
 package vtech.iot.mesh.sim.examples;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JPanel;
-import javax.swing.Timer;
-
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.ValueAxis;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.time.DynamicTimeSeriesCollection;
-import org.jfree.data.time.Second;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 
 import vtech.iot.mesh.sim.MeshSim;
 import vtech.iot.mesh.sim.domain.devices.AlohaDevice;
 
-public class AlohaTransmitters extends ApplicationFrame {
-  private static final String TITLE = "Aloha Simulation";
-
-  private static final float Y_MAX = 100;
-  private static final float Y_MIN = 0;
-  private static final int COUNT = 2 * 60;
-  private static final int FAST = 1000;
-  private Timer timer;
-
+public class AlohaTransmitters extends AbstractGraphExample {
+  final static String WINDOW_TITLE = "Aloha Simulation";
+  final static String GRAPH_TITLE = "Aloha Simulation: one station, 10 requests (average) per second";
+  
   private MeshSim meshSim = new MeshSim();
 
-  public AlohaTransmitters(final String title) {
-    super(title);
+  public AlohaTransmitters() {
+    super(WINDOW_TITLE);
 
     meshSim.addDevice(new AlohaDevice(10));
-    meshSim.addDevice(new AlohaDevice(10));
-
-    final DynamicTimeSeriesCollection dataset = new DynamicTimeSeriesCollection(1, COUNT, new Second());
-    dataset.setTimeBase(new Second(0, 0, 0, 1, 1, 2011));
-    dataset.addSeries(new float[] {}, 0, "Network load");
-    JFreeChart chart = createChart(dataset);
-
-    this.add(new ChartPanel(chart), BorderLayout.CENTER);
-    JPanel btnPanel = new JPanel(new FlowLayout());
-
-    this.add(btnPanel, BorderLayout.SOUTH);
-
-    timer = new Timer(FAST, new ActionListener() {
-
-      float[] newData = new float[1];
-
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        newData[0] = (float) meshSim.getMediumBusyPercentage();
-        dataset.advanceTime();
-        dataset.appendData(newData);
-      }
-    });
   }
 
-  private JFreeChart createChart(final XYDataset dataset) {
-    final JFreeChart result = ChartFactory.createTimeSeriesChart(TITLE, "hh:mm:ss", "[%]", dataset, true, true, false);
-    final XYPlot plot = result.getXYPlot();
-    ValueAxis domain = plot.getDomainAxis();
-    domain.setAutoRange(true);
-    ValueAxis range = plot.getRangeAxis();
-    range.setRange(Y_MIN, Y_MAX);
-    range.setAutoRange(true);
-    return result;
+  @Override
+  protected String getGraphTitle() {
+    return GRAPH_TITLE;
   }
 
-  public void start() {
-    timer.start();
+  @Override
+  protected int getSeriesCount() {
+    return 1;
+  }
+
+  @Override
+  protected String getSeriesName(int seriesIndex) {
+    if (seriesIndex == 0) {
+      return "Network load";
+    }
+
+    return "Unknown";
+  }
+
+  @Override
+  protected float getSeriesData(int seriesIndex) {
+    if (seriesIndex == 0) {
+      return (float)meshSim.getMediumBusyPercentage();
+    }
+
+    return 1000;
   }
 
   public static void main(final String[] args) {
@@ -83,11 +52,10 @@ public class AlohaTransmitters extends ApplicationFrame {
 
       @Override
       public void run() {
-        AlohaTransmitters demo = new AlohaTransmitters(TITLE);
+        AlohaTransmitters demo = new AlohaTransmitters();
         demo.pack();
         RefineryUtilities.centerFrameOnScreen(demo);
         demo.setVisible(true);
-        demo.start();
       }
     });
   }
