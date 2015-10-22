27a75685e75ff6722fc88aa51fdb36ca2268e5a9
Witold Markowski <witold.a.markowski@gmail.com>
2015-10-22 23:48:50 +0200

    Rework aloha transmitter.

--- src/main/java/vtech/sim/iot/mesh/aloha/AlohaTransmitter.java
+++ src/main/java/vtech/sim/iot/mesh/aloha/AlohaTransmitter.java
@@ -3,6 +3,7 @@ package vtech.sim.iot.mesh.aloha;
 import java.util.ArrayList;
 import java.util.List;
 
+import vtech.sim.core.Event;
 import vtech.sim.core.Process;
 import vtech.sim.iot.mesh.Medium;
 import vtech.sim.iot.mesh.Packet;
@@ -11,12 +12,15 @@ import vtech.sim.iot.mesh.Transmitter;
 
 public class AlohaTransmitter extends Process implements Transmitter {
 
-  private final static int IDLE = 0;
-  private final static int BEGIN_TRANSMISSION = 1;
-  private final static int FINISH_TRANSMISSION = 2;
+  private final static int STATE_IDLE = 0;
+  private final static int STATE_TRANSMITTING = 1;
+
+  private final static int EVENT_NEW_PACKET_TO_SEND = 0;
+  private final static int EVENT_PACKET_SENT = 1;
 
   private Medium medium;
   private List<Packet> packets = new ArrayList<Packet>();
+  private int state = STATE_IDLE;
 
   public AlohaTransmitter(Medium medium) {
     super();
@@ -25,42 +29,56 @@ public class AlohaTransmitter extends Process implements Transmitter {
   }
 
   @Override
-  public void execute() {
-    switch (getPhase()) {
-    case IDLE:
-      break;
-    case BEGIN_TRANSMISSION:
-      if (packets.size() == 0) {
-        setPhase(IDLE);
+  public void execute(Event event) {
+    switch (state) {
+    case STATE_IDLE:
+      if (event.getEventType() == EVENT_INIT) {
         return;
       }
 
-      Packet packet = packets.remove(0);
-      Transmission transmission = medium.sendPacket(packet);
+      if (event.getEventType() == EVENT_NEW_PACKET_TO_SEND) {
+        Packet packet = packets.remove(0);
+        Transmission transmission = medium.sendPacket(packet);
 
-      setPhase(FINISH_TRANSMISSION);
-      scheduleNextExecution(transmission.getTransmissionDurationInMillis());
-      break;
-    case FINISH_TRANSMISSION:
-      if (packets.size() == 0) {
-        setPhase(IDLE);
+        state = STATE_TRANSMITTING;
+        scheduleNextExecution(transmission.getTransmissionDurationInMillis(), EVENT_PACKET_SENT);
+
         return;
       }
 
-      setPhase(BEGIN_TRANSMISSION);
-      scheduleNextExecutionToNow();
+      throw new IllegalStateException();
+    case STATE_TRANSMITTING:
+      if (event.getEventType() == EVENT_NEW_PACKET_TO_SEND) {
+        return;
+      }
 
-      break;
+      if (event.getEventType() == EVENT_PACKET_SENT) {
+
+        if (packets.size() == 0) {
+          state = STATE_IDLE;
+          return;
+        }
+
+        Packet packet = packets.remove(0);
+        Transmission transmission = medium.sendPacket(packet);
+
+        state = STATE_TRANSMITTING;
+        scheduleNextExecution(transmission.getTransmissionDurationInMillis(), EVENT_PACKET_SENT);
+        return;
+      }
+
+      throw new IllegalStateException();
     }
+  }
+
+  @Override
+  public void execute() {
+
   }
 
   @Override
   public void addPacketToSend(Packet packet) {
     packets.add(packet);
-
-    if (getPhase() == IDLE) {
-      setPhase(BEGIN_TRANSMISSION);
-      scheduleNextExecutionToNow();
-    }
+    scheduleNextExecutionToNow(EVENT_NEW_PACKET_TO_SEND);
   }
 }
