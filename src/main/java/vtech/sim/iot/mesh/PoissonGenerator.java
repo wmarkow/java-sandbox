b609566f0571608ab60d06a521c8a09fd39d3075
Witold Markowski <witold.a.markowski@gmail.com>
2015-10-26 20:57:47 +0100

    Refactor.

--- src/main/java/vtech/sim/iot/mesh/PoissonGenerator.java
+++ src/main/java/vtech/sim/iot/mesh/PoissonGenerator.java
@@ -1,67 +1,24 @@
 package vtech.sim.iot.mesh;
 
-import vtech.sim.core.Event;
-import vtech.sim.core.Process;
+public class PoissonGenerator extends TrafficGenerator {
 
-public class PoissonGenerator extends Process {
-
-  private final static int STATE_IDLE = 0;
-  private final static int STATE_WAIT = 1;
-  private final static int STATE_GENERATE_PACKET = 2;
-
-  private final static int EVENT_WAIT_FINISHED = 0;
-  private final static int EVENT_GENERATE_PACKET = 1;
-
-  private Transmitter transmitter;
   private RandomGenerator poisson;
   private double averageRequestsPerSecond;
-  private int state = STATE_IDLE;
 
   public PoissonGenerator(Transmitter transmitter, double averageRequestsPerSecond) {
-    super();
+    super(transmitter);
 
-    this.transmitter = transmitter;
     poisson = new RandomGenerator();
     this.averageRequestsPerSecond = averageRequestsPerSecond;
   }
 
   @Override
-  public void execute(Event event) {
-    switch (state) {
-    case STATE_IDLE:
-      if (event.getEventType() == EVENT_INIT) {
+  protected double getMillisToNextRequest() {
+    return poisson.getPoissonMillisToNextRequest(averageRequestsPerSecond);
+  }
 
-        double nexMillisToNextRequest = poisson.getPoissonMillisToNextRequest(averageRequestsPerSecond);
-        state = STATE_WAIT;
-
-        scheduleNextExecution(nexMillisToNextRequest, EVENT_WAIT_FINISHED);
-
-        return;
-      }
-      
-      throw new IllegalStateException();
-    case STATE_WAIT:
-      if (event.getEventType() == EVENT_WAIT_FINISHED) {
-        state = STATE_GENERATE_PACKET;
-        scheduleNextExecutionToNow(EVENT_GENERATE_PACKET);
-
-        return;
-      }
-      
-      throw new IllegalStateException();
-    case STATE_GENERATE_PACKET:
-      if (event.getEventType() == EVENT_GENERATE_PACKET) {
-        transmitter.addPacketToSend(new Packet());
-
-        double nexMillisToNextRequest = poisson.getPoissonMillisToNextRequest(averageRequestsPerSecond);
-        state = STATE_WAIT;
-
-        scheduleNextExecution(nexMillisToNextRequest, EVENT_WAIT_FINISHED);
-        
-        return;
-      }
-      
-      throw new IllegalStateException();
-    }
+  @Override
+  protected Packet getNextPacketToSend() {
+    return new Packet();
   }
 }
---
+++ src/main/java/vtech/sim/iot/mesh/TrafficGenerator.java
@@ -0,0 +1,63 @@
+package vtech.sim.iot.mesh;
+
+import vtech.sim.core.Event;
+import vtech.sim.core.Process;
+
+public abstract class TrafficGenerator extends Process {
+  private final static int STATE_IDLE = 0;
+  private final static int STATE_WAIT = 1;
+  private final static int STATE_GENERATE_PACKET = 2;
+
+  private final static int EVENT_WAIT_FINISHED = 0;
+  private final static int EVENT_GENERATE_PACKET = 1;
+
+  private Transmitter transmitter;
+  private int state = STATE_IDLE;
+
+  public TrafficGenerator(Transmitter transmitter) {
+    super();
+
+    this.transmitter = transmitter;
+  }
+  
+  @Override
+  final public void execute(Event event) {
+    switch (state) {
+    case STATE_IDLE:
+      if (event.getEventType() == EVENT_INIT) {
+
+        state = STATE_WAIT;
+
+        scheduleNextExecution(getMillisToNextRequest(), EVENT_WAIT_FINISHED);
+
+        return;
+      }
+      
+      throw new IllegalStateException();
+    case STATE_WAIT:
+      if (event.getEventType() == EVENT_WAIT_FINISHED) {
+        state = STATE_GENERATE_PACKET;
+        scheduleNextExecutionToNow(EVENT_GENERATE_PACKET);
+
+        return;
+      }
+      
+      throw new IllegalStateException();
+    case STATE_GENERATE_PACKET:
+      if (event.getEventType() == EVENT_GENERATE_PACKET) {
+        transmitter.addPacketToSend(getNextPacketToSend());
+
+        state = STATE_WAIT;
+
+        scheduleNextExecution(getMillisToNextRequest(), EVENT_WAIT_FINISHED);
+        
+        return;
+      }
+      
+      throw new IllegalStateException();
+    }
+  }
+  
+  protected abstract double getMillisToNextRequest();
+  protected abstract Packet getNextPacketToSend();
+}
