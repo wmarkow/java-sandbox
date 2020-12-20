79f814d7a3588833fbda120b3a1b814387094819
Witold Markowski <witold.a.markowski@gmail.com>
2020-12-20 17:54:08 +0000

    Add more examples to half-duplex complex simulation comparison.

---
+++ src/main/java/vtech/sim/iot/mesh/halfduplex/examples/HalfDuplexComplex128Simulation.java
@@ -0,0 +1,20 @@
+package vtech.sim.iot.mesh.halfduplex.examples;
+
+import vtech.sim.iot.mesh.MeshSimulation;
+import vtech.sim.iot.mesh.halfduplex.HalfDuplexComplexDevice;
+
+public class HalfDuplexComplex128Simulation extends MeshSimulation {
+
+  @Override
+  protected void prepareDevices() {
+    for (int q = 0; q < 128; q++) {
+      addDevice(new HalfDuplexComplexDevice(10, getMedium()));
+    }
+  }
+
+  public static void main(String[] args) {
+    HalfDuplexComplex128Simulation sim = new HalfDuplexComplex128Simulation();
+    sim.init();
+    sim.start();
+  }
+}
---
+++ src/main/java/vtech/sim/iot/mesh/halfduplex/examples/HalfDuplexComplex32Simulation.java
@@ -0,0 +1,20 @@
+package vtech.sim.iot.mesh.halfduplex.examples;
+
+import vtech.sim.iot.mesh.MeshSimulation;
+import vtech.sim.iot.mesh.halfduplex.HalfDuplexComplexDevice;
+
+public class HalfDuplexComplex32Simulation extends MeshSimulation {
+
+  @Override
+  protected void prepareDevices() {
+    for (int q = 0; q < 32; q++) {
+      addDevice(new HalfDuplexComplexDevice(10, getMedium()));
+    }
+  }
+
+  public static void main(String[] args) {
+    HalfDuplexComplex32Simulation sim = new HalfDuplexComplex32Simulation();
+    sim.init();
+    sim.start();
+  }
+}
