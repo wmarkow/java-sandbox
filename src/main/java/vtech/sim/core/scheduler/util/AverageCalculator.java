4ba96bc7375c1d2a9cb4487b6ba12cf71914b3d2
Witold Markowski <witold.a.markowski@gmail.com>
2020-12-20 20:02:10 +0000

    Introduce an average value calculator.

---
+++ src/main/java/vtech/sim/core/scheduler/util/AverageCalculator.java
@@ -0,0 +1,22 @@
+package vtech.sim.core.scheduler.util;
+
+public class AverageCalculator
+{
+    private double sumOfValues = 0;
+    private double countOfValues = 0;
+
+    public AverageCalculator() {
+
+    }
+
+    public synchronized void addValue(double value)
+    {
+        sumOfValues += value;
+        countOfValues++;
+    }
+
+    public double getAverage()
+    {
+        return sumOfValues / countOfValues;
+    }
+}
