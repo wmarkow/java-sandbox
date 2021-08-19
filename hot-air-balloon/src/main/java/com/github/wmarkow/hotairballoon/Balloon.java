f39ba8d23377bd449710e89e66a1fa48486ca546
Witold Markowski <witold.a.markowski@gmail.com>
2021-08-19 21:15:36 +0100

    [HotAirBalloon] Sterowanie z klawiatury

--- hot-air-balloon/src/main/java/com/github/wmarkow/hotairballoon/Balloon.java
+++ hot-air-balloon/src/main/java/com/github/wmarkow/hotairballoon/Balloon.java
@@ -8,14 +8,28 @@ import javax.swing.JLabel;
 
 public class Balloon extends JLabel {
 
+	private int x;
+	private int y;
+
 	public Balloon() {
 		super();
 		this.setMinimumSize(new Dimension(64, 128));
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		g.setColor(Color.RED);
 		g.fillRect(0, 0, 64, 128);
+	}
+
+	public void setX(int x) {
+		this.x = x;
+
+		this.setBounds(x, y, 64, 128);
+	}
+
+	public void setY(int y) {
+		this.y = y;
+		this.setBounds(x, y, 64, 128);
 	}
 }
--- hot-air-balloon/src/main/java/com/github/wmarkow/hotairballoon/HotAirBalloon.java
+++ hot-air-balloon/src/main/java/com/github/wmarkow/hotairballoon/HotAirBalloon.java
@@ -1,25 +1,76 @@
 package com.github.wmarkow.hotairballoon;
 
 import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFrame;
 
 public class HotAirBalloon {
 
 	private static Balloon balloon;
-	
+
+	private static Timer timer;
+	private static TimerTask timerTask;
+
+	private static boolean leftArrowPressed = false;
+	private static boolean rightArrowPressed = false;
+
 	private static void createAndShowGUI() {
 		// Create and set up the window.
 		JFrame frame = new JFrame("Hot Air Balloon");
 		frame.setMinimumSize(new Dimension(1024, 768));
 		frame.setLayout(null);
 		balloon = new Balloon();
-		balloon.setBounds(0,0,64,128);
+		balloon.setX(512);
+		balloon.setY(500);
+
 		frame.getContentPane().add(balloon);
 		frame.pack();
 		frame.setVisible(true);
+		frame.addKeyListener(new KeyListener() {
 
-		
+			@Override
+			public void keyTyped(KeyEvent e) {
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+					leftArrowPressed = true;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+					rightArrowPressed = true;
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+					leftArrowPressed = false;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+					rightArrowPressed = false;
+				}
+			}
+		});
+
+		int requiredFps = 30;
+		long period = 1000 / requiredFps;
+
+		timer = new Timer();
+		timerTask = new TimerTask() {
+
+			@Override
+			public void run() {
+				onFrame();
+			}
+		};
+
+		timer.scheduleAtFixedRate(timerTask, 0, period);
 	}
 
 	public static void main(String[] args) {
@@ -31,4 +82,26 @@ public class HotAirBalloon {
 			}
 		});
 	}
+	
+	private static void onFrame() {
+		if (rightArrowPressed == true) {
+			int newX = balloon.getX() + 10;
+			if (newX > 1024 - 64) {
+				newX = 1024 - 64;
+			}
+			balloon.setX(newX);
+
+		}
+
+		if (leftArrowPressed == true) {
+			int newX = balloon.getX() - 10;
+			if (newX < 0) {
+				newX = 0;
+			}
+			balloon.setX(newX);
+		}
+	}
 }
+
+//error: Kiedy przytrzymam myszke i klikne strzalke, to moge sterowac okienkiem
+//pojawia sie czerwony prostokat i mozna nim sterowac strzalkami: prawa i lewa
