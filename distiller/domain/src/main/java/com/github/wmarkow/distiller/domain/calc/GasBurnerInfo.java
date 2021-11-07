0e17658f14f86e78608da9f787ccbdf69d23791a
Witold Markowski <witold.a.markowski@gmail.com>
2021-11-07 20:22:35 +0000

    [Distiller] Prerliminary work on gas burner calculations.

---
+++ distiller/domain/src/main/java/com/github/wmarkow/distiller/domain/calc/GasBurnerInfo.java
@@ -0,0 +1,6 @@
+package com.github.wmarkow.distiller.domain.calc;
+
+public class GasBurnerInfo {
+    private double minFlow;
+    private double maxFlow;
+}
---
+++ distiller/domain/src/main/java/com/github/wmarkow/distiller/domain/calc/GasHeaterCalc.java
@@ -0,0 +1,8 @@
+package com.github.wmarkow.distiller.domain.calc;
+
+public class GasHeaterCalc {
+
+    public double calculateHeatingPower(GasBurnerInfo burner, ) {
+
+    }
+}
