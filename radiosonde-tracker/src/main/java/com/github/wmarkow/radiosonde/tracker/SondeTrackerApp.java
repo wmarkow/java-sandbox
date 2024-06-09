8469b2a6013c44e0d83a366c062184d663a92bb9
Witold Markowski <witold.a.markowski@gmail.com>
2024-06-09 11:44:39 +0100

    [RadioSondeTracker] Add basic example to open world map from SHP file using GeoTools Java library.

---
+++ radiosonde-tracker/src/main/java/com/github/wmarkow/radiosonde/tracker/SondeTrackerApp.java
@@ -0,0 +1,49 @@
+package com.github.wmarkow.radiosonde.tracker;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
+import org.geotools.swing.JMapFrame;
+import org.geotools.swing.data.JFileDataStoreChooser;
+
+public class SondeTrackerApp
+{
+    private static final Logger LOGGER = org.geotools.util.logging.Logging.getLogger( SondeTrackerApp.class );
+
+    /**
+     * GeoTools Quickstart demo application. Prompts the user for a shapefile and displays its contents on the
+     * screen in a map frame
+     */
+    public static void main( String[] args ) throws Exception
+    {
+        // display a data store file chooser dialog for shapefiles
+        LOGGER.info( "Quickstart" );
+        LOGGER.config( "Welcome Developers" );
+        LOGGER
+            .info( "java.util.logging.config.file=" + System.getProperty( "java.util.logging.config.file" ) );
+
+        File file = new File("src/main/resources/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp");
+        
+        FileDataStore store = FileDataStoreFinder.getDataStore( file );
+        SimpleFeatureSource featureSource = store.getFeatureSource();
+
+        // Create a map content and add our shapefile to it
+        MapContent map = new MapContent();
+        map.setTitle( "Quickstart" );
+
+        Style style = SLD.createSimpleStyle( featureSource.getSchema() );
+        Layer layer = new FeatureLayer( featureSource, style );
+        map.addLayer( layer );
+
+        // Now display the map
+        JMapFrame.showMap( map );
+    }
+}
