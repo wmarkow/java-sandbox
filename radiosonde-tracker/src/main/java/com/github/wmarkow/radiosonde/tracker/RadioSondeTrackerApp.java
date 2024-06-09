6128e60e71486fe1627e9d90fa8b88854e9b30bd
Witold Markowski <witold.a.markowski@gmail.com>
2024-06-09 17:29:31 +0100

    [RadioSondeTracker] Add worst case scenario prediction.

--- radiosonde-tracker/src/main/java/com/github/wmarkow/radiosonde/tracker/RadioSondeTrackerApp.java
+++ radiosonde-tracker/src/main/java/com/github/wmarkow/radiosonde/tracker/RadioSondeTrackerApp.java
@@ -1,6 +1,7 @@
 package com.github.wmarkow.radiosonde.tracker;
 
 import java.awt.Color;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,7 @@ import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.MapContent;
+import org.geotools.referencing.GeodeticCalculator;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.geotools.styling.FeatureTypeStyle;
 import org.geotools.styling.Graphic;
@@ -48,6 +50,7 @@ public class RadioSondeTrackerApp
         MapContent map = new MapContent();
         map.addLayer( new TileLayer( service ) );
         map.addLayer( prepareSondeMapLayer() );
+        map.addLayer( prepareSondePredictionMapLayer() );
 
         JMapFrame.showMap( map );
     }
@@ -57,13 +60,16 @@ public class RadioSondeTrackerApp
         CsvReader csvReader = new CsvReader();
         ArrayList< DataPoint > dataPoints =
             csvReader.readDataPoints( "src/main/resources/sondes/V3742166/V3742166.csv" );
+//        ArrayList< DataPoint > dataPoints =
+//          csvReader.readDataPoints( "src/main/resources/sondes/V3742167/V3742167.csv" );
+        
 
         final SimpleFeatureType TYPE = createFeatureType();
 
         List< SimpleFeature > featureList = new ArrayList< SimpleFeature >();
         GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
 
-        for( int q = 0; q < dataPoints.size(); q++ )
+        for( int q = 0; q < dataPoints.size(); q+=10 )
         {
             DataPoint dp = dataPoints.get( q );
 
@@ -77,6 +83,57 @@ public class RadioSondeTrackerApp
 
         ListFeatureCollection lfc = new ListFeatureCollection( TYPE, featureList );
         FeatureLayer layer = new FeatureLayer( lfc, createPointStyle() );
+
+        return layer;
+    }
+
+    public static FeatureLayer prepareSondePredictionMapLayer() throws SchemaException
+    {
+        CsvReader csvReader = new CsvReader();
+        ArrayList< DataPoint > dataPoints =
+            csvReader.readDataPoints( "src/main/resources/sondes/V3742166/V3742166.csv" );
+//        ArrayList< DataPoint > dataPoints =
+//            csvReader.readDataPoints( "src/main/resources/sondes/V3742167/V3742167.csv" );
+
+        final SimpleFeatureType TYPE = createFeatureType();
+
+        List< SimpleFeature > featureList = new ArrayList< SimpleFeature >();
+        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
+
+        GeodeticCalculator calc = new GeodeticCalculator( DefaultGeographicCRS.WGS84 );
+
+        for( int q = 0; q < dataPoints.size(); q++ )
+        {
+            DataPoint dp = dataPoints.get( q );
+
+            if( dp.climbing_m_s >= 0 )
+            {
+                continue;
+            }
+            if(dp.altitude_m > 1500)
+            {
+                continue;
+            }
+
+            // Sonde is falling down. Let's predict its landing point.
+            double time_h = dp.altitude_m / Math.abs( dp.climbing_m_s ) / 3600.0;
+            double distance_km = dp.speed_km_h * time_h;
+            double distance_m = distance_km * 1000;
+
+            calc.setStartingGeographicPoint( dp.longitude, dp.latitude );
+            calc.setDirection( dp.course, distance_m );
+            Point2D dstPoint = calc.getDestinationGeographicPoint();
+
+            /* Longitude (= x coord) first ! */
+            Point point = geometryFactory.createPoint( new Coordinate( dstPoint.getX(), dstPoint.getY() ) );
+            SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder( TYPE );
+            featureBuilder.add( point );
+            SimpleFeature feature = featureBuilder.buildFeature( null );
+            featureList.add( feature );
+        }
+
+        ListFeatureCollection lfc = new ListFeatureCollection( TYPE, featureList );
+        FeatureLayer layer = new FeatureLayer( lfc, createPredictionPointStyle() );
 
         return layer;
     }
@@ -110,6 +167,37 @@ public class RadioSondeTrackerApp
             styleFactory.createStroke( filterFactory.literal( Color.BLUE ), filterFactory.literal( 2 ) ) );
 
         mark.setFill( styleFactory.createFill( filterFactory.literal( Color.CYAN ) ) );
+
+        gr.graphicalSymbols().clear();
+        gr.graphicalSymbols().add( mark );
+        gr.setSize( filterFactory.literal( 10 ) );
+
+        /*
+         * Setting the geometryPropertyName arg to null signals that we want to draw the default geomettry of
+         * features
+         */
+        PointSymbolizer sym = styleFactory.createPointSymbolizer( gr, null );
+
+        Rule rule = styleFactory.createRule();
+        rule.symbolizers().add( sym );
+        FeatureTypeStyle fts = styleFactory.createFeatureTypeStyle( new Rule[]
+        { rule } );
+        Style style = styleFactory.createStyle();
+        style.featureTypeStyles().add( fts );
+
+        return style;
+    }
+
+    private static Style createPredictionPointStyle()
+    {
+        Graphic gr = styleFactory.createDefaultGraphic();
+
+        Mark mark = styleFactory.getCircleMark();
+
+        mark.setStroke(
+            styleFactory.createStroke( filterFactory.literal( Color.RED ), filterFactory.literal( 2 ) ) );
+
+        mark.setFill( styleFactory.createFill( filterFactory.literal( Color.RED ) ) );
 
         gr.graphicalSymbols().clear();
         gr.graphicalSymbols().add( mark );
