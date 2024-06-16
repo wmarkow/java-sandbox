77151f3fc1f667a39a602ef72437ff480ef6e248
Witold Markowski <witold.a.markowski@gmail.com>
2024-06-16 17:52:23 +0100

    [RadioSondeTracker] Use commons-math3 library to make prediction easier.

--- radiosonde-tracker/src/main/java/com/github/wmarkow/radiosonde/tracker/domain/AdvancedLandingPointPredictor.java
+++ radiosonde-tracker/src/main/java/com/github/wmarkow/radiosonde/tracker/domain/AdvancedLandingPointPredictor.java
@@ -1,8 +1,11 @@
 package com.github.wmarkow.radiosonde.tracker.domain;
 
 import java.awt.geom.Point2D;
+import java.util.Comparator;
 import java.util.logging.Logger;
 
+import org.apache.commons.math3.analysis.interpolation.LinearInterpolator;
+import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 import org.geotools.geometry.DirectPosition2D;
 import org.geotools.referencing.GeodeticCalculator;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
@@ -39,36 +42,21 @@ public class AdvancedLandingPointPredictor
         double altitude_m = dataPoint.altitude_m;
         final double climb_m_s = dataPoint.climbing_m_s; // assuming it is negative
         final double dt_s = 10; // in seconds
-        final double daccuracy_m = 50.0; // in meters
 
-        int counter = 0;
         while( altitude_m > 0.0 )
         {
-            DataPoint dpAtAltitude = dataSet.getFirstDataPointByAltitude( altitude_m, daccuracy_m );
+            double windSpeedAtAltitude_km_h = dataSet.getWindSpeed( altitude_m );
+            double windCourseAtAltitude = dataSet.getWindCourse( altitude_m );
+
             altitude_m = altitude_m + climb_m_s * dt_s;
-
-            if( dpAtAltitude == null )
-            {
-                LOGGER.warning( String.format( "Can't fint DataPoint for altitude %s with accuracy %s",
-                    altitude_m, daccuracy_m ) );
-                counter++;
-
-                continue;
-            }
-
-            double distance_m = dpAtAltitude.speed_km_h * 1000.0 / 3600.0 * dt_s;
+            double distance_m = windSpeedAtAltitude_km_h * 1000.0 / 3600.0 * dt_s;
 
             // calculate new longitude and latitude
             calc.setStartingGeographicPoint( lon, lat );
-            calc.setDirection( dpAtAltitude.course, distance_m );
+            calc.setDirection( windCourseAtAltitude, distance_m );
             Point2D newPoint = calc.getDestinationGeographicPoint();
             lon = newPoint.getX();
             lat = newPoint.getY();
-        }
-        if( counter > 0 )
-        {
-            LOGGER.warning(
-                String.format( "Couldn't find %s DataPoints in total for this prediction.", counter ) );
         }
 
         return new DirectPosition2D( lon, lat );
